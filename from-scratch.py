3d53bc4b83dfa2ac130f420a71d7277019125270
Sam Fowler <sgfowler98@gmail.com>
2019-10-13 23:49:00 -0400

    Fixed problem with open() not using utf8 decoding, added output of words sorted by 'goodness', moved class data member definitions to constructor, fixed typo in function definition, and changed styling to match PEP 8

--- from-scratch.py
+++ from-scratch.py
@@ -11,39 +11,41 @@ DATASET = 'Eminem.csv'
     If it is not positive, we define it as negative.
 '''
 
+
 class NaiveClassifier:
     def __init__(self):
-        pass
+        self.wordDict = {}
+        self.neg_num, self.pos_num = 0, 0
+        self.total = 0
 
     def load_data(self, filename):
         illegalWords = set(['katy', 'perry', 'katyperry', 'psy', 'shakira', 'eminem'])
-        self.wordDict = {}
-        self.neg_num, self.pos_num  = 0, 0
-        with open(filename, "r") as f:
+        with open(filename, 'r', encoding='utf8') as f:
             reader = csv.reader(f)
             for row in reader:
                 if row[0] == 'COMMENT_ID':
                     continue
                 content = re.sub(r'[^\w\s]', '', row[3]).lower()
+
                 content = content.split(" ")
                 for word in content:
                     if word in illegalWords:
                         continue
                     if not word in self.wordDict:
                         self.wordDict[word] = Word(word)
-                    if int(row[4]) == 1: # its spam
+                    if int(row[4]) == 1:  # its spam
                         self.wordDict[word].positive += 1
                         self.pos_num += 1
                     else:
                         self.wordDict[word].negative += 1
                         self.neg_num += 1
 
-        self.total = len(self.wordDict.keys())
-        #pos_num, neg_num = len(pos_set), len(neg_set)
+        self.total += len(self.wordDict.keys())
+        # pos_num, neg_num = len(pos_set), len(neg_set)
         for word in self.wordDict:
             self.wordDict[word].positive = self.laplace(self.total, self.pos_num, self.wordDict[word].positive)
             self.wordDict[word].negative = self.laplace(self.total, self.neg_num, self.wordDict[word].negative)
-        
+
     def predict(self, msg):
         data = re.sub(r'[^\w\s]', '', msg).lower().split(" ")
         pos, neg = 1, 1
@@ -62,24 +64,32 @@ class NaiveClassifier:
     def laplace(self, total, unique, val):
         return (1 + val) / (total + unique)
 
+
 class Word:
     def __init__(self, key):
         self.key = key
         self.negative = 0
         self.positive = 0
-    
+
     def __hash__(self):
         return self.key
-    
+
     def prob(self):
         pass
 
     def __eq__(self, rhs):
         return self.key == rhs.key
 
-def test_classifer(classifier, filename):
+    def __str__(self):
+        return f'-{self.negative}, +{self.positive}'
+
+    def __lt__(self, other):
+        return (self.negative - self.positive) < (other.negative - other.positive)
+
+
+def test_classifier(classifier, filename):
     wrong, correct = 0, 0
-    with open(filename, "r") as f:
+    with open(filename, "r", encoding='utf8') as f:
         reader = csv.reader(f)
         for row in reader:
             if row[0] == 'COMMENT_ID':
@@ -91,15 +101,17 @@ def test_classifer(classifier, filename):
                 correct += 1
             else:
                 wrong += 1
-    
+
     print(f'prediction accuracy: {correct/(wrong + correct)}')
+
 
 if __name__ == "__main__":
     c = NaiveClassifier()
     c.load_data(DIR+DATASET)
-    #c.load_data(DIR+"Shakira.csv")
-    #c.load_data(DIR+'LMFAO.csv')
-    #c.load_data(DIR+"Psy.csv")
+    c.load_data(DIR+"Shakira.csv")
+    c.load_data(DIR+'LMFAO.csv')
+    c.load_data(DIR+"Psy.csv")
 
-    test_classifer(c, DIR+'KatyPerry.csv')
-    
+    test_classifier(c, DIR + 'KatyPerry.csv')
+    for k in sorted(c.wordDict, key=c.wordDict.get, reverse=True):
+        print('"' + str(k) + '": ' + str(c.wordDict[k]))
