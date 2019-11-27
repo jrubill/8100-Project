7cbf76cf2c06789ee916d451e91ff0d1ea20abca
jrubill <jrubill@clemson.edu>
2019-11-26 20:52:40 -0500

    updated multiple

--- project1/multiple.py
+++ project1/multiple.py
@@ -6,6 +6,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn import svm
 # CNN
 class CNN(object):
@@ -35,7 +36,7 @@ class RFClassifier():
     def train(self):
         x_train, x_test, y_train, y_test = train_test_split(self.x, self.y, test_size = 0.2)
 
-        self.classifier = RandomForestClassifier(n_estimators=15, criterion='entropy', n_jobs=-1)
+        self.classifier = RandomForestClassifier(n_estimators=15, criterion='entropy', n_jobs=1)
         self.classifier.fit(x_train, y_train)
         predRF = self.classifier.predict(x_test)
         from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
@@ -72,11 +73,12 @@ class SVM():
         self.y = le.fit_transform(self.y)
     
     def train(self):
-        x_train, x_test, y_train, y_test = train_test_split(self.x, self.y, test_size = 0.2)
-
-        self.classifier = svm.SVC(C=1.0, kernel='linear')
+        x_train, x_test, y_train, y_test = train_test_split(self.x, self.y,test_size = 0.1)
+        #x_train, y_train = self.x, self.y
+        # self.classifier = svm.SVC(C=1.0, kernel='linear')
+        self.classifier = svm.SVC(kernel='linear')
         self.classifier.fit(x_train, y_train)
-        predRF = self.classifier.predict(x_test)
+        #predRF = self.classifier.predict(x_test)
         from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
         '''
         print('Accuracy score: {}'.format(accuracy_score(y_test, predRF)))
@@ -93,8 +95,7 @@ class Voting():
         from sklearn.linear_model import LogisticRegression
         from sklearn.naive_bayes import GaussianNB
         from sklearn.ensemble import VotingClassifier
-        #self.classifiers = [('lr', LogisticRegression(solver='lbfgs')), ('gnb',GaussianNB()), ('rf', svm.SVC())]
-        self.classifiers=[('svm',svm.SVC())]
+        self.classifiers=[('mlp',MLPClassifier()), ('rf', RandomForestClassifier(n_estimators=5)), ('svm', svm.SVC(kernel='linear'))]
         self.classifier = VotingClassifier(estimators=self.classifiers, voting='hard', n_jobs=-1)
         self.vect = CountVectorizer(max_features=1500)
     
@@ -130,10 +131,43 @@ class Voting():
         text = self.vect.transform([text]).todense()
         return self.classifier.predict(text)
 
-if __name__ == "__main__":
+class MLP():
+    def __init__(self):
+        self.classifier = MLPClassifier()
+        self.vect = CountVectorizer(max_features=1500)
     
-    clf = Voting()
-    clf.load_data('datasets/KatyPerry.csv')
-    clf.train()
-    print(clf.classify("yeet"))
+    def load_data(self, filename):
+        self.x = []
+        self.y = []
+        with open(filename, "r") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                if row[0] == 'COMMENT_ID':
+                    continue
+                content = re.sub(r'[^\w\s]', '', row[3]).lower()
+                content = content.split(" ")
+                self.y.append(int(row[4]))
+                self.x.append( ' '.join(content))
+        self.x = self.vect.fit_transform(self.x).toarray()
+        le = LabelEncoder()
+        self.y = le.fit_transform(self.y)
+    
+    def train(self):
+        x_train, x_test, y_train, y_test = train_test_split(self.x, self.y, test_size = 0.2)
+
+        self.classifier.fit(x_train, y_train)
+        predRF = self.classifier.predict(x_test)
+        from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+        '''
+        print('Accuracy score: {}'.format(accuracy_score(y_test, predRF)))
+        print('Precision score: {}'.format(precision_score(y_test, predRF)))
+        print('Recall score: {}'.format(recall_score(y_test, predRF)))
+        print('F1 score: {}'.format(f1_score(y_test, predRF)))
+        '''
+    def classify(self, text):
+        text = self.vect.transform([text]).todense()
+        return self.classifier.predict(text)
+
+if __name__ == "__main__":
+    pass
     
